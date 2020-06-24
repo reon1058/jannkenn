a2a431e7ac05ef0a690c53aa16cfcca25c8d430f
reon1058 <67031793+reon1058@users.noreply.github.com>
2020-06-24 12:06:08 +0900

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,27 @@
 radio.onReceivedNumber(function (receivedNumber) {
     basic.clearScreen()
-    basic.pause(200)
-    if (receivedNumber == 1) {
-        basic.showIcon(IconNames.Happy)
-    } else if (receivedNumber == 2) {
-        basic.showIcon(IconNames.No)
-    } else {
+    basic.pause(1000)
+    if (receivedNumber == relaese && (receivedNumber == relaese2 && relaese == relaese2)) {
+        basic.showIcon(IconNames.Triangle)
+    } else if (receivedNumber == 0 && (relaese == 1 && relaese2 == 2)) {
+        basic.showIcon(IconNames.Triangle)
+    } else if (receivedNumber == 0 && relaese == 2 && relaese2 == 1) {
+        basic.showIcon(IconNames.Triangle)
+    } else if (receivedNumber == 1 && (relaese == 0 && relaese2 == 2)) {
+        basic.showIcon(IconNames.Triangle)
+    } else if (receivedNumber == 1 && (relaese == 2 && relaese2 == 0)) {
+        basic.showIcon(IconNames.Triangle)
+    } else if (receivedNumber == 2 && (relaese == 0 && relaese2 == 1)) {
+        basic.showIcon(IconNames.Triangle)
+    } else if (receivedNumber == 2 && (relaese == 1 && relaese2 == 0)) {
         basic.showIcon(IconNames.Triangle)
     }
 })
 input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Target)
     radio.sendNumber(0)
+    relaese = 0
+    relaese2 = 0
 })
 input.onGesture(Gesture.Shake, function () {
     basic.showIcon(IconNames.Heart)
@@ -19,11 +29,17 @@ input.onGesture(Gesture.Shake, function () {
 input.onButtonPressed(Button.AB, function () {
     basic.showIcon(IconNames.Square)
     radio.sendNumber(2)
+    relaese = 2
+    relaese2 = 2
 })
 input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.Scissors)
     radio.sendNumber(1)
+    relaese = 1
+    relaese2 = 1
 })
+let relaese2 = 0
+let relaese = 0
 radio.setGroup(167)
 basic.forever(function () {
 	
